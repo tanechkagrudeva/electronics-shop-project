73950fe61620e6975af271aad50dc92280e67cad
Татьяна <tanechka201286grudeva@gmail.com>
2023-04-08 19:28:35 +0300

    Added tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,19 +3,17 @@ from src.item import Item
 
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 
-
-def testInit():
-    itm=Item("Утюг", 1000, 20)
-    assert itm.name == "Утюг"
-    assert itm.price == 1000
-    assert itm.quantity == 20
 @pytest.fixture
-def itm():
-    return "Утюг", 1000, 20
-def test_calculate_total_price_(itm):
-    assert itm.calculate_total_price(itm) == 20000
+def item_obj():
+    return Item('Утюг', 1000, 20)
+def test_init(item_obj):
+    assert item_obj.name == "Утюг"
+    assert item_obj.price == 1000
+    assert item_obj.quantity == 20
 
-def test_apply_discount(itm):
-    pay_rate = 0.5
-    assert itm.apply_discount(itm) == 500
+def test_calculate_total_price(item_obj):
+    assert item_obj.calculate_total_price() == 20000
+
+def test_apply_discount(item_obj):
+    assert item_obj.apply_discount() == 1000
 
